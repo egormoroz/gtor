cb9106c558d3e8f2c0443e299eafef42f2499fd7
Egor Moroz <egormoroz2020@gmail.com>
2022-09-19 19:08:33 +0300

    hotfix

--- simplex.py
+++ simplex.py
@@ -1,4 +1,5 @@
 import numpy as np
+from pysmps import smps_loader as smps
 
 '''
 Minimize <c, x>, s.t. Ax <= b
@@ -10,6 +11,7 @@ Shapes:
     b: (n, 1)
 '''
 def solve(c, A, b):
+    EPS = 1e-12
     assert np.all(b >= 0)
 
     n, m = A.shape
@@ -18,11 +20,12 @@ def solve(c, A, b):
     D = np.hstack((A, np.eye(n), b))
     T = np.hstack((-c.T, np.zeros((1, n + 1))))
 
-    pivot_col = np.argmin(T)
+    pivot_col = np.argmax(T)
     pivot_val = T[0, pivot_col]
 
-    while pivot_val < -1e-8:
-        pivot_row = np.argmin(D[:, -1] / D[:, pivot_col])
+    while pivot_val >= 0.5:
+        mask = np.where(D[:, pivot_col] > EPS)[0]
+        pivot_row = mask[(D[mask, -1] / D[mask, pivot_col]).argmin()]
         basis[pivot_row] = pivot_col
 
         D[pivot_row, :] /= D[pivot_row, pivot_col]
@@ -33,16 +36,17 @@ def solve(c, A, b):
                 continue
             D[i, :] -= D[i, pivot_col] * D[pivot_row, :]
 
-        pivot_col = np.argmin(T)
+        pivot_col = np.argmax(T)
         pivot_val = T[0, pivot_col]
+        print(T[0, -1], pivot_val)
 
     x = np.zeros(n+m)
     np.put(x, basis, D[:, -1])
-    return (x[:m], T[0, -1])
+    return (x[:m].reshape((m, 1)), T[0, -1])
 
-c = np.array([[2, 3]]).T
-A = np.array([[1, 1], [6, 3], [1, 2]])
-b = np.array([[100, 360, 120]]).T
+# c = np.array([[2, 3]]).T
+# A = np.array([[1, 1], [6, 3], [1, 2]])
+# b = np.array([[100, 360, 120]]).T
 #The solution is (40, 40)
 
 # c = np.array([[1, 2, -1]]).T
@@ -50,5 +54,22 @@ b = np.array([[100, 360, 120]]).T
 # b = np.array([[14, 28, 30]]).T
 #The solution is (5, 4, 0)
 
-print(solve(c, A, b))
+# print(solve(c, A, b))
+
+#mps = smps.load_mps('gen-ip002.mps')
+mps = smps.load_mps('reblock115.mps')
+
+c = mps[6]
+A = mps[7]
+b = mps[9]['rhs']
+
+c = c.reshape((c.shape[0], 1))
+b = b.reshape((b.shape[0], 1))
+
+x, cx = solve(c, A, b)
+
+print(x)
+
+print(f'{cx} vs {c.T @ x}')
+print(np.max(A @ x - b))
 
