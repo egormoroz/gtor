f304ba466f883b0db4075a0555e055774149ce43
Egor Moroz <egormoroz2020@gmail.com>
2022-10-02 19:47:22 +0300

    asdfasdf

--- simplex.py
+++ simplex.py
@@ -1,75 +1,61 @@
 import numpy as np
 from pysmps import smps_loader as smps
+import time
 
-'''
-Minimize <c, x>, s.t. Ax <= b
-
-Shapes:
-    c: (m, 1)
-    x: (m, 1)
-    A: (n, m)
-    b: (n, 1)
-'''
-def solve(c, A, b):
-    EPS = 1e-12
-    assert np.all(b >= 0)
-
-    n, m = A.shape
-
+def simplex(D, T, n, m):
     basis = np.arange(start=m, stop=n+m, step=1, dtype=int)
-    D = np.hstack((A, np.eye(n), b))
-    T = np.hstack((-c.T, np.zeros((1, n + 1))))
 
-    pivot_col = np.argmax(T)
-    pivot_val = T[0, pivot_col]
+    k = 0
+    while True:
+        #eblan?
+        # x = np.zeros(n+m)
+        # np.put(x, basis, D[:, -1])
+        # print(k, T[-1] - c @ x[:m], np.max(A @ x[:m] - b),
+        #         np.max((D[:,:-1] @ x) - D[:,-1]), T[-1])
 
-    while pivot_val >= 0.5:
-        mask = np.where(D[:, pivot_col] > EPS)[0]
-        pivot_row = mask[(D[mask, -1] / D[mask, pivot_col]).argmin()]
-        basis[pivot_row] = pivot_col
+        pc = np.argmax(T[:-1])
+        if T[pc] < 0.5:
+            break
 
-        D[pivot_row, :] /= D[pivot_row, pivot_col]
-        T -= T[0, pivot_col] * D[pivot_row, :]
+        mask = np.where(D[:, pc] > 1e-8)[0]
+        pr = mask[np.argmin(D[mask, -1] / D[mask, pc])]
 
-        for i in range(n):
-            if i == pivot_row:
-                continue
-            D[i, :] -= D[i, pivot_col] * D[pivot_row, :]
+        basis[pr] = pc
 
-        pivot_col = np.argmax(T)
-        pivot_val = T[0, pivot_col]
-        print(T[0, -1], pivot_val)
+        D[pr] /= D[pr, pc]
+        T -= T[pc] * D[pr]
+
+        for i in range(D.shape[0]):
+            if i != pr:
+                D[i] -= D[i, pc] * D[pr]
+
+        k += 1
+        print('{:04d} {}'.format(k, T[-1]))
+
 
     x = np.zeros(n+m)
     np.put(x, basis, D[:, -1])
-    return (x[:m].reshape((m, 1)), T[0, -1])
+    print(T[-1] - c @ x[:m], np.max(A @ x[:m] - b),
+            np.max((D[:,:-1] @ x) - D[:,-1]), T[-1])
 
-# c = np.array([[2, 3]]).T
-# A = np.array([[1, 1], [6, 3], [1, 2]])
-# b = np.array([[100, 360, 120]]).T
-#The solution is (40, 40)
+    return (x[:m], T[-1])
 
-# c = np.array([[1, 2, -1]]).T
-# A = np.array([[2, 1, 1], [4, 2, 3], [2, 5, 5]])
-# b = np.array([[14, 28, 30]]).T
-#The solution is (5, 4, 0)
 
-# print(solve(c, A, b))
-
-#mps = smps.load_mps('gen-ip002.mps')
 mps = smps.load_mps('reblock115.mps')
 
 c = mps[6]
 A = mps[7]
 b = mps[9]['rhs']
 
-c = c.reshape((c.shape[0], 1))
-b = b.reshape((b.shape[0], 1))
+n, m = A.shape
 
-x, cx = solve(c, A, b)
+D = np.hstack((A, np.eye(n), b[:, np.newaxis]))
+T = np.hstack((-c, np.zeros(n + 1)))
 
-print(x)
 
-print(f'{cx} vs {c.T @ x}')
-print(np.max(A @ x - b))
+
+start = time.time()
+x, z = simplex(D, T, n, m)
+end = time.time()
+print('elapsed', end - start)
 
