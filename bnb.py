ee9da4d3657fe36ed5f43a80b41adf567167ff64
egormoroz <egormoroz2020@gmail.com>
2022-11-13 02:42:18 +0300

    branch and bound finally works

--- bnb.py
+++ bnb.py
@@ -1,59 +1,236 @@
-from dualsimplex import *
-from collections import deque
+from simplex import *
 import time
+from pysmps import smps_loader
+from heapq import *
+from itertools import count
 
-'''
-- solves integer linear program
-- returns first found feasible solution
-'''
-def branch_and_bound(A, b, c, s):
+
+def read_mps(file):
+    m = smps_loader.load_mps(file)
+    I = np.array([i == 'integral' for i in m[4]])
+    s_dict = { 'E': EQ, 'L': LE, 'G': GE }
+    s = np.array([s_dict[i] for i in m[5]])
+    c = m[6]
+    A = m[7]
+    b = next(iter(m[9].values()))
+
+
+    lo, up = None, None
+    if m[11]:
+        bnd = next(iter(m[11].values()))
+
+        if 'LO' in bnd:
+            lo = bnd['LO']
+            lo_mask = lo > 0
+            lo_vals = lo
+            lo = lo_mask, lo_vals
+
+        if 'UP' in bnd:
+            up = bnd['UP']
+            up_mask = np.isfinite(up)
+            up_vals = up
+            up = up_mask, up_vals
+
+    return A, b, c, s, I, lo, up
+
+
+def solve_relaxation(A, b, c, s, lo, up, fixed):
+    lo_mask, lo_vals = lo 
+    up_mask, up_vals = up
+
+    fixed_mask, fixed_vals = fixed
+    old_fv = fixed_vals
+    fixed_vals = fixed_vals[fixed_mask]
+
+    m, n, fixed_z = len(b), len(c), c[fixed_mask] @ fixed_vals
+    any_fixed = len(fixed_vals) > 0
+
+    if any_fixed:
+        not_fixed = ~fixed_mask
+        lo_mask = lo_mask & not_fixed
+        up_mask = up_mask & not_fixed
+
+    A = np.vstack((A, np.eye(n)[lo_mask, :], np.eye(n)[up_mask, :]))
+    b = np.hstack((b, lo_vals[lo_mask], up_vals[up_mask]))
+    s = np.hstack((s, GE * np.ones(np.sum(lo_mask)), 
+                   LE * np.ones(np.sum(up_mask))))
+
+    if any_fixed:
+        b[:m] -= A[:m, fixed_mask] @ fixed_vals
+        A[:m, fixed_mask] = 0
+        c = np.copy(c)
+        c[fixed_mask] = 0
+
+    sln = two_phase_simplex(A, b, c, s)
+    if sln:
+        x, z = sln
+        x[fixed_mask] = fixed_vals
+
+        assert np.all(lo_vals[lo_mask] - 1e-8 <= x[lo_mask])
+        assert np.all(x[up_mask] <= up_vals[up_mask] + 1e-8)
+
+        return x, z + fixed_z
+
+    return None
+
+
+def tighten_up(i, bnd, up):
+    up_mask, up_vals = np.copy(up[0]), np.copy(up[1])
+    up_mask[i] = True
+    up_vals[i] = min(up_vals[i], bnd)
+
+    return up_mask, up_vals
+
+
+def tighten_lo(i, bnd, lo):
+    lo_mask, lo_vals = np.copy(lo[0]), np.copy(lo[1])
+    lo_mask[i] = True
+    lo_vals[i] = max(lo_vals[i], bnd)
+
+    return lo_mask, lo_vals
+
+
+def fix_var(i, fx, fixed):
+    fixed_mask, fixed_vals = np.copy(fixed[0]), np.copy(fixed[1])
+    fixed_mask[i] = True
+    fixed_vals[i] = fx
+
+    return fixed_mask, fixed_vals
+
+
+def bnb_v2(A, b, c, s, I, lo=None, up=None, target_gap=1e-8):
     m, n = A.shape
+    EPS = 1e-8
 
-    A = np.vstack((A, np.eye(n)))
-    b = np.hstack((b, np.zeros(n)))
-    s = np.hstack((s, GE * np.ones(n)))
+    if lo is None:
+        lo = np.array([True] * n), np.zeros(n)
+    if up is None:
+        up = np.array([False] * n), np.ones(n) * np.inf
+    fixed = np.array([False] * n), np.zeros(n)
 
-    q = deque()
-    q.append((np.zeros(n), GE * np.ones(n)))
+    counter = count()
+    q = []
+    q.append((n, -1e20, next(counter), lo, up, fixed))
+
+    if I is None:
+        I = np.array([True] * n)
+
+    best_sol, primal_bound = None, 1e20
 
     while q:
-        b[-n:], s[-n:] = q.popleft()
-        sln = dual_simplex(A, b, c, s)
-        if sln is None:
-            #relaxed LP is infeasible
-            continue
-        x, z = sln
+        n_vars, dual_bound, k, lo, up, fixed = heappop(q)
 
-        non_ints, = np.where(np.abs(x[:n] - np.round(x[:n])) > 1e-8)
+        sln = solve_relaxation(A, b, c, s, lo, up, fixed)
+        if sln is None:
+            continue
+
+        x, z = sln
+        if z >= primal_bound:
+            continue
+
+        nonint_mask = np.abs(x - np.round(x)) > EPS
+        non_ints = np.where(nonint_mask & I)[0]
+
+        if k % 1000 == 0:
+            print(k, z, n_vars, len(non_ints))
+
+        lo_mask, lo_vals = lo
+        up_mask, up_vals = up
+        fixed_mask, fixed_vals = fixed
+
         if len(non_ints) == 0:
-            print('done')
-            return x[:n], z
+            if best_sol is None or z < primal_bound:
+                best_sol, primal_bound = x, z
+
+                gap = primal_bound - dual_bound
+                gap /= max(EPS, min(abs(primal_bound), 
+                                      abs(dual_bound)))
+
+                print('** {} {} {:.2e}'.format('haha', z, gap), k)
+                #print(np.vstack((np.where(lo_mask), lo_vals[lo_mask])))
+                #print(np.vstack((np.where(up_mask), up_vals[up_mask])))
+                #print(np.vstack((np.where(fixed_mask), fixed_vals[fixed_mask])))
+                if gap < target_gap:
+                    break
+            continue
+
+        assert np.all(lo_vals[lo_mask] - 1e-8 <= x[lo_mask])
+        assert np.all(x[up_mask] <= up_vals[up_mask] + 1e-8)
+
 
         for i in non_ints:
-            bb, ss = np.copy(b[-n:]), np.copy(s[-n:])
-            bb[-i], ss[-i] = np.floor(x[i]), LE
-            q.append((bb, ss))
-            bb, ss = np.copy(b[-n:]), np.copy(s[-n:])
-            bb[-i], ss[-i] = np.ceil(x[i]), GE
-            q.append((bb, ss))
+            bnd = np.floor(x[i])
+            if lo_vals[i] <= bnd and bnd <= up_vals[i]:
+                if bnd - lo_vals[i] <= EPS:
+                    new_fixed = fix_var(i, bnd, fixed)
+                    heappush(q, (n_vars - 1, z, next(counter), lo, up, new_fixed))
+                else:
+                    new_up = tighten_up(i, bnd, up)
+                    heappush(q, (n_vars, z, next(counter), lo, new_up, fixed))
+            bnd += 1
+            if lo_vals[i] <= bnd and bnd <= up_vals[i]:
+                if up_vals[i] - bnd <= EPS:
+                    new_fixed = fix_var(i, bnd, fixed)
+                    heappush(q, (n_vars - 1, z, next(counter), lo, up, new_fixed))
+                else:
+                    new_lo = tighten_lo(i, bnd, lo)
+                    heappush(q, (n_vars, z, next(counter), new_lo, up, fixed))
 
-    print('failed')
-    return x, z
+
+    return best_sol, primal_bound, gap
 
 
 def main():
-    A = np.array([[-2, 2], [-8, 10]])
-    b = np.array([1, 13])
-    c = -np.ones(2)
-    s = np.array([GE, LE])
+    #A = np.array([[-2, 2], [-8, 10]])
+    #b = np.array([1, 13])
+    #c = -np.ones(2)
+    #s = np.array([GE, LE])
+    #I = np.array([True, True])
 
+    weight = np.array([10, 20, 30])
+    cost = np.array([60, 100, 120])
+
+    A = np.array(
+        [[ 10, 20, 30],
+         [  1,  0,  0],
+         [  0,  1,  0],
+         [  0,  0,  1]])
+    b = np.array([50, 1, 1, 1])
+    c = -cost
+    s = np.array([LE] * 4)
+    I = np.array([True] * 3)
+
+    up = np.array([True] * 3), np.ones(3)
+
+    bnb_v2(A, b, c, s, I, up=up)
+
+    '''
+
+    branch_and_bound(A, b, c, s, I)
 
     start = time.time()
-    x, z = branch_and_bound(A, b, c, s)
+
+    x, z, gap = branch_and_bound(A, b, c, s, I)
     end = time.time()
-    print(x, z)
+
     print('elapsed', end - start)
 
+    print(x, z, gap)
+
+    '''
+
 if __name__ == '__main__':
-    main()
+    np.set_printoptions(edgeitems=30, linewidth=100000)
+
+    A, b, c, s, I, lo, up = read_mps('gen-ip016.mps')
+    bnb_v2(A, b, c, s, I, lo=lo, up=up)
+    #sln = solve_relaxation(A, b, c, s, lo, up)
+    #print(sln)
+
+
+    #args = read_mps('flugpl.mps')
+    #args = read_mps('ej.mps')
+    
+    #branch_and_bound(*args)
+    #main()
 
--- simplex.py
+++ simplex.py
@@ -1,20 +1,27 @@
 import numpy as np
-from pysmps import smps_loader as smps
-import time
+from pysmps import smps_loader
 
-def simplex(D, T, n, m):
-    basis = np.arange(start=m, stop=n+m, step=1, dtype=int)
+LE = -1
+EQ = 0
+GE = 1
 
-    k = 0
+def read_mps(file):
+    m = smps_loader.load_mps(file)
+    I = np.array([i == 'integral' for i in m[4]])
+    s_dict = { 'E': EQ, 'L': LE, 'G': GE }
+    s = np.array([s_dict[i] for i in m[5]])
+    c = m[6]
+    A = m[7]
+    b = next(iter(m[9].values()))
+
+    return A, b, c, s, I
+
+
+# minimizes
+def simplex(D, T, basis):
     while True:
-        #eblan?
-        # x = np.zeros(n+m)
-        # np.put(x, basis, D[:, -1])
-        # print(k, T[-1] - c @ x[:m], np.max(A @ x[:m] - b),
-        #         np.max((D[:,:-1] @ x) - D[:,-1]), T[-1])
-
         pc = np.argmax(T[:-1])
-        if T[pc] < 0.5:
+        if T[pc] < 1e-8:
             break
 
         mask = np.where(D[:, pc] > 1e-8)[0]
@@ -29,33 +36,133 @@ def simplex(D, T, n, m):
             if i != pr:
                 D[i] -= D[i, pc] * D[pr]
 
-        k += 1
-        print('{:04d} {}'.format(k, T[-1]))
+    return D, T, basis
 
 
-    x = np.zeros(n+m)
-    np.put(x, basis, D[:, -1])
-    print(T[-1] - c @ x[:m], np.max(A @ x[:m] - b),
-            np.max((D[:,:-1] @ x) - D[:,-1]), T[-1])
+def construct_table(A, b, s):
+    m, n = A.shape
+    assert len(b) == m
+    assert len(s) == m
 
-    return (x[:m], T[-1])
+    neg = b < 0
+    if np.any(neg):
+        A[neg, :] *= -1
+        b[neg] *= -1
+        s[neg] *= -1
+
+    ics = np.argsort(s)
+    A, b, s = A[ics], b[ics], s[ics]
+
+    eqs = np.where(s == EQ)[0]
+    les = np.where(s == LE)[0]
+    ges = np.where(s == GE)[0]
+
+    # TODO: first phase is not always neccessary
+    # test whether np.zeros() is a feasible starting solution
+
+    n_eq, n_le, n_ge = len(eqs), len(les), len(ges)
+
+    # slack variables
+    SL = np.zeros((m, n_le))
+    SL[les, np.arange(n_le)] = 1
+
+    # excess variables
+    EX = np.zeros((m, n_ge))
+    EX[ges, np.arange(n_ge)] = -1
+
+    # artificial variables
+    n_avs = n_eq + n_ge
+    AR = np.zeros((m, n_avs))
+    av_rows = np.hstack((eqs, ges))
+    AR[av_rows, np.arange(n_avs)] = 1
+
+    A = np.hstack((A, SL, EX, AR))
+    c = np.zeros(A.shape[1])
+    c[:-n_avs] = np.sum(A[av_rows, :-n_avs], axis=0)
+
+    D = np.hstack((A, b[:, np.newaxis]))
+    T = np.hstack((c, np.sum(b[av_rows])))
+
+    basis = np.zeros(m, dtype=np.int64)
+    basis[:n_le] = np.arange(n_le) + n
+    basis[n_le:] = np.arange(n_avs) + A.shape[1] - n_avs
+
+    return (D, T, basis), n_avs
+
+def first_phase(A, b, c, s):
+    tbl, n_avs = construct_table(A, b, s)
+    D, T, basis = simplex(*tbl)
+
+    # infeasible
+    if T[-1] > 1e-8:
+        return
+
+    total_vars = len(T) - 1
+    basis_avs = basis[basis >= total_vars - n_avs]
+
+    T[:] = 0
+    T[:len(c)] = -c
+    for i, j in enumerate(basis):
+        T -= T[j] * D[i]
+
+    # no artificial variables in basis
+    if len(basis_avs) == 0:
+        T, T[-1] = T[:len(T) - n_avs], T[-1]
+        D = np.hstack((D[:, :-n_avs-1], D[:, -1, np.newaxis]))
+        return D, T, basis
+
+    # a few artificial variables in basis
+    nonneg_org_vars = np.where(T[:-1] >= -1e-8)[0]
+    vars_remain = np.hstack((nonneg_org_vars, basis_avs, -1))
+
+    remap = np.zeros(len(T) - 1, dtype=np.int64)
+    remap[vars_remain] = np.arange(len(vars_remain))
+
+    T = T[vars_remain]
+    D = D[:, vars_remain]
+
+    return D, T, remap[basis]
 
 
-mps = smps.load_mps('reblock115.mps')
+def two_phase_simplex(A, b, c, s):
+    m, n = A.shape
+    assert len(b) == m
+    assert len(s) == m
+    assert len(c) == n
 
-c = mps[6]
-A = mps[7]
-b = mps[9]['rhs']
+    tbl = first_phase(A, b, c, s)
+    if tbl is None:
+        #print('infeasible')
+        return
 
-n, m = A.shape
+    D, T, basis = simplex(*tbl)
 
-D = np.hstack((A, np.eye(n), b[:, np.newaxis]))
-T = np.hstack((-c, np.zeros(n + 1)))
+    x = np.zeros(D.shape[1])
+    x[basis] = D[:, -1]
+
+    return x[:len(c)], T[-1]
 
 
+def main():
+    # A = np.array([[3, 1], [4, 3], [1, 2]])
+    # b = np.array([3, 6, 4])
+    # c = np.array([4, 1])
+    # s = [EQ, GE, LE]
 
-start = time.time()
-x, z = simplex(D, T, n, m)
-end = time.time()
-print('elapsed', end - start)
+    #A = np.array([[1, 1], [2, 1], [1, 2], [1, 1]])
+    #b = np.array([3, 5, 5, 1])
+    #c = -np.array([2, 3])
+    #s = np.array([LE, LE, LE, GE])
+
+    A, b, c, s, _ = read_mps('flugpl.mps')
+    print(two_phase_simplex(A, b, c, s))
+
+if __name__ == "__main__":
+    A, b, c, s, _ = read_mps('flugpl.relaxed.mps')
+    #A = np.array([[3, 1], [4, 3], [1, 2]])
+    #b = np.array([3, 6, 4])
+    #c = np.array([4, 1])
+    #s = np.array([EQ, GE, LE])
+
+    print(two_phase_simplex(A, b, c, s))
 
